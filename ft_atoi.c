387cb51a456aba819c7e3d402b5885d4ddcdd6ad
Fair Danny <fdanny@mi-p3.msk.21-school.ru>
2021-05-11 21:34:59 +0300

    ft_atoi-add 999...999

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,37 +6,60 @@
 /*   By: fdanny <fdanny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 16:06:17 by fdanny            #+#    #+#             */
-/*   Updated: 2021/05/11 16:25:49 by fdanny           ###   ########.fr       */
+/*   Updated: 2021/05/11 21:31:22 by fdanny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+size_t	ft_spcrm(const char *str)
+{
+	size_t	p;
+
+	p = 0;
+	while (str[p] == ' ' || str[p] == '\t' || str[p] == '\n'
+		|| str[p] == '\v' || str[p] == '\f' || str[p] == '\r')
+		p++;
+	return (p);
+}
+
+int	ft_check_edge(int edge, int sign, unsigned long result)
+{
+	if (edge != 0)
+		sign *= result;
+	else
+	{
+		if (sign == 1)
+			sign = -1;
+		else
+			sign = 0;
+	}
+		return (sign);
+}
 
 int	ft_atoi(const char *str)
 {
 	size_t			p;
-	long long int	sign;
-	long long		result;
+	int				sign;
+	unsigned long	result;
+	int				edge;
 
 	sign = 1;
-	p = 0;
+	edge = 20;
+	p = ft_spcrm(str);
 	result = 0;
-	while (str[p] == ' ' || str[p] == '\t' || str[p] == '\n'
-		|| str[p] == '\v' || str[p] == '\f' || str[p] == '\r')
-		p++;
 	if (str[p] == '-')
 		sign = -1;
 	if (str[p] == '-' || str[p] == '+')
 		p++;
 	else if (str[p] == '+')
 		p++;
-	while (ft_isdigit(str[p]))
+	while (ft_isdigit(str[p]) && edge != 0)
 	{
 		result += str[p] - '0';
 		if (ft_isdigit(str[p + 1]))
 			result *= 10;
+		edge--;
 		p++;
 	}
-	sign *= result;
-	return (sign);
+	return (ft_check_edge(edge, sign, result));
 }
