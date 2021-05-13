35d70f07963131c4367a19dc520870c38fc1aaec
Fair Danny <fdanny@oa-n5.msk.21-school.ru>
2021-05-13 13:55:39 +0300

    Принудительный segfault при haystack == NULL

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fdanny <fdanny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 16:05:36 by fdanny            #+#    #+#             */
-/*   Updated: 2021/05/12 19:49:07 by fdanny           ###   ########.fr       */
+/*   Updated: 2021/05/13 13:47:47 by fdanny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_isneedle(const char *hs, const char *nd)
 	size_t	i;
 
 	i = 0;
-	while (*(hs + i) == *(nd + i) || *(nd + i) == '\0')
+	while (*(hs + i) == *(nd + i) || *(nd + i) == '\0' || hs == 0)
 	{
 		if (*(nd + i) == '\0')
 			return (1);
@@ -29,12 +29,16 @@ int	ft_isneedle(const char *hs, const char *nd)
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	p;
+	char	*error;
 
-	if (haystack == NULL || needle == NULL)
-		return ((char *)haystack);
+	if (haystack == NULL)
+	{
+		error = (char *)0;
+		*error = 1;
+	}
 	if (ft_strlen(needle) == 0)
 		return ((char *)haystack);
-	else if (ft_strlen(needle) > len || ft_strlen(needle) > ft_strlen(haystack))
+	else if (ft_strlen(needle) > len)
 		return (NULL);
 	p = 0;
 	while (p <= len - ft_strlen(needle))
